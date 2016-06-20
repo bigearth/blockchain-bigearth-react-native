dd89b9d88e6d2dedcff32de03001a24c861eb17a
Carlos Cardona <cgcardona@gmail.com>
2016-06-19 20:06:07 -0700

    Fetch data for homepage hero.

--- homepage-hero.js
+++ homepage-hero.js
@@ -5,18 +5,53 @@ import {
 } from 'react-native';
 
 class HomepageHero extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+      all: 21000000,
+      current: 0,
+      perc: 0,
+      difficulty: 0,
+      next_difficulty: 0,
+      next_difficulty_perc: 0,
+      retarget_in: 0
+    };
+    fetch('https://stageblockchain.bigearth.io/coin.json')
+    .then((response) => response.text())
+    .then((responseText) => {
+      var data = JSON.parse(responseText);
+      this.setState({
+        data: data.data,
+        value: data.data.markets.coinbase.value,
+        all: data.data.volume.all,
+        current: data.data.volume.current,
+        perc: data.data.volume.perc,
+        market_cap: data.data.markets.coinbase.value * data.data.volume.current,
+        difficulty: data.data.last_block.difficulty,
+        next_difficulty: data.data.next_difficulty.difficulty,
+        next_difficulty_perc: data.data.next_difficulty.perc,
+        retarget_in: data.data.next_difficulty.retarget_in
+      });
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
 	render() {    
     return (
     	<View>
       	<Text>Bitcoin Markets</Text>
-        <Text>Market Value: $ per coin</Text>
-        <Text> /  coins mined</Text>
-        <Text>% of total coins</Text>
-        <Text>Market Cap $</Text>
+        <Text>Market Value: ${this.state.value} per coin</Text>
+        <Text>{this.state.current} / {this.state.all}  coins mined</Text>
+        <Text>%{this.state.perc} of total coins</Text>
+        <Text>Market Cap ${this.state.market_cap}</Text>
         <Text>Bitcoin Difficulty</Text>
+        <Text>{this.state.difficulty}</Text>
         <Text>Difficulty is a measure of how difficult it is to find a new block below a given target.</Text>
-        <Text>Next Difficulty (estimate) change to current</Text> 
-        <Text> blocks until difficulty changes</Text>
+        <Text>Next Difficulty (estimate) {this.state.next_difficulty}</Text>
+        <Text>{this.state.next_difficulty_perc} change to current</Text> 
+        <Text>{this.state.retarget_in} blocks until difficulty changes</Text>
       </View>
     )
   }
--- homepage.js
+++ homepage.js
@@ -10,29 +10,6 @@ import HomepageBlocks from './homepage-blocks.js';
 class Homepage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: 0,
-      all: 21000000,
-      current: 0,
-      perc: 0,
-      difficulty: 0,
-      next_difficulty: 0,
-      next_difficulty_perc: 0,
-      retarget_in: 0
-    };
-    fetch('https://stageblockchain.bigearth.io/coin.json')
-    .then((response) => response.text())
-    .then((responseText) => {
-      var data = JSON.parse(responseText);
-      console.log(data.data.coin.name);
-      this.setState({
-        value: data.data.markets.coinbase.value
-      });
-
-    })
-    .catch((error) => {
-      console.warn(error);
-    });
   }
   _navigate(name) {
   	this.props.navigator.push({
@@ -45,9 +22,8 @@ class Homepage extends Component {
 	render() {    
     return (
     	<View>
-        <HomepageHero url="https://stageblockchain.bigearth.io/coin.json" />
+        <HomepageHero />
         <HomepageBlocks url="https://stageblockchain.bigearth.io/blocks/416614,416613,416612,416611,416610,416609,416608,416607,416606,416605,416604,416603,416602,416601,416600,416599,416598,416597,416596,416595.json" />
-        <Text>Market Value: ${this.state.value} per coin</Text>
         <TouchableHighlight onPress={ () => this._navigate('asdf') }>
           <Text>Go to the block page</Text>
         </TouchableHighlight>
